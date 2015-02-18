50de02076bce925448203eff0245d0a119dd006b
The Dude <u6du65@gmail.com>
2015-02-18 23:55:09 +0200

    Getting closer to have a working build. Added the downloading of NDK/SDK. The Cocos2djs's cmake build script is the main issue currently.

--- vagrantimage/bootstrap.sh
+++ vagrantimage/bootstrap.sh
@@ -6,6 +6,8 @@ sudo apt-get update
 sudo apt-get install -y python-software-properties
 sudo apt-get install -y software-properties-common
 sudo apt-get install -y make
+sudo apt-get install -y ant
+sudo apt-get install build-essential -y
 
 # add the custom apt repository for java
 # sudo add-apt-repository -y ppa:webupd8team/java
@@ -44,10 +46,43 @@ sudo apt-get install -y libglfw-dev
 
 sudo apt-get install unzip
 
-wget http://www.cocos2d-x.org/filedown/cocos2d-x-3.4.zip
-wget http://www.cocos2d-x.org/filedown/cocos2d-js-v3.3.zip
+# Installing the NDK/SDK 9
+# strangely enough the 9th NDK downloads faster the the 10th
+# =======================================================
 
-unzip cocos2d-js-v3.3.zip
-unzip cocos2d-x-3.4.zip
+wget http://dl.google.com/android/ndk/android-ndk-r9d-linux-x86.tar.bz2
+tar xvfj android-ndk-r9d-linux-x86.tar.bz2
+echo 'export NDK_ROOT=/home/vagrant/android-ndk-r9d' >> ~/.bashrc
 
-cd cocos2d-x-3.4
+
+wget http://dl.google.com/android/android-sdk_r24.0.2-linux.tgz
+tar zxvf android-sdk_r24.0.2-linux.tgz
+echo 'export ANDROID_SDK_ROOT=/home/vagrant/android-sdk-linux' >> ~/.bashrc
+
+
+		# Installing the ndk10 which is not compatible with the current cocos2djs
+		# wget http://dl.google.com/android/ndk/android-ndk-r10d-linux-x86.bin
+		# sudo chmod a+x android-ndk-r10d-linux-x86.bin
+		# sudo ./android-ndk-r10d-linux-x86.bin
+
+
+		# wget http://www.cocos2d-x.org/filedown/cocos2d-x-3.4.zip
+		# wget https://github.com/cocos2d/cocos2d-x/archive/cocos2d-x-3.4.zip
+		# wget http://www.cocos2d-x.org/filedown/cocos2d-js-v3.3.zip
+
+		# quite possibly that only this thingie is needed to make it work
+
+wget https://github.com/cocos2d/cocos2d-js/archive/Cocos2d-JS-v3.3-final.zip
+
+unzip Cocos2d-JS-v3.3-final.zip
+mv cocos2d-js-Cocos2d-JS-v3.3-final/ cocosjs-3.3
+
+cd cocosjs-3.3
+sudo ./setup.py
+
+
+
+# unzip cocos2d-js-v3.3.zip
+# unzip cocos2d-x-3.4.zip
+
+# sudo bash cocos2d-x-3.4/build/install-deps-linux.sh
